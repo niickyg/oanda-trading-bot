54694897dee8ba634e4e35f07ba4e80e79d03f27
Nick G <nickyg1028@gmail.com>
2025-07-14 08:17:47 -0600

    Fix StrategyTriArb constructor to accept config and register it in strategy package

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -5,6 +5,7 @@ Expose strategy plugins and legacy utilities.
 
 from .macd_trends import MACDTrendStrategy
 from .rsi_reversion import StrategyRSIReversion
+from .tri_arb import StrategyTriArb
 
 # Legacy utilities from the strategy utils module
 from .utils import sl_tp_levels, update_strategy_performance
@@ -14,4 +15,5 @@ __all__ = [
     "StrategyRSIReversion",
     "sl_tp_levels",
     "update_strategy_performance",
+    "StrategyTriArb",
 ]
