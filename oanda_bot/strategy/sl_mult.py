21dd5611adbc6d1fafb1417031237c535f37370a
Nick G <nickyg1028@gmail.com>
2025-07-16 05:53:23 -0600

    fix sl mult error

--- oanda_bot/strategy/sl_mult.py
+++ oanda_bot/strategy/sl_mult.py
@@ -21,7 +21,26 @@ def generate_signals(data: pd.DataFrame,
 
 
 # Parameter grid for research tuning
+
 param_grid = {
     'stop_loss_multiplier': [1.0, 1.5, 2.0, 2.5],
     'atr_window': [10, 14, 20]
 }
+
+
+# Strategy class wrapper
+class Strategysl_mult:
+    """
+    Stop-loss multiplier strategy: wraps generate_signals and exposes param_grid.
+    """
+    name = "sl_mult"
+
+    def __init__(self, stop_loss_multiplier: float = 1.5, atr_window: int = 14):
+        self.stop_loss_multiplier = stop_loss_multiplier
+        self.atr_window = atr_window
+
+    @staticmethod
+    def generate_signals(data: pd.DataFrame, stop_loss_multiplier: float = 1.5, atr_window: int = 14) -> pd.Series:
+        return generate_signals(data, stop_loss_multiplier, atr_window)
+
+    param_grid = param_grid
